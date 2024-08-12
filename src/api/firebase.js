f2d803b197eabd515c17c58b6182075e3e8b7693
DEV.JS <pztdrd@nate.com>
2024-08-12 15:41:37 +0900

    add CartPage

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,7 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getDatabase, ref, get, set } from "firebase/database";
+import { getDatabase, ref, get, set, remove } from "firebase/database";
 import { v4 as uuid } from "uuid";
 
 const firebaseConfig = {
@@ -21,14 +21,17 @@ const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const database = getDatabase(app);
 
+//로그인
 export function login() {
   signInWithPopup(auth, provider).catch(console.error);
 }
 
+//로그아웃
 export function logout() {
   signOut(auth).catch(console.error);
 }
 
+//사용자의 로그인아웃 정보 요청
 export function onUserStateChange(callback) {
   onAuthStateChanged(auth, async (user) => {
     // 1. 사용자가 있는 경우에 (로그인한경우)
@@ -37,6 +40,7 @@ export function onUserStateChange(callback) {
   });
 }
 
+//사용자 권한확인
 async function adminUser(user) {
   // 2. 사용자가 어드민 권한을 가지고 있는지 확인!
   // 3. {...user, isAdmin: true/false}
@@ -51,6 +55,7 @@ async function adminUser(user) {
     });
 }
 
+//관리자 상품등록
 export async function addNewProduct(product, image) {
   const id = uuid();
   return set(ref(database, `products/${id}`), {
@@ -60,4 +65,31 @@ export async function addNewProduct(product, image) {
     image,
     options: product.options.split(","),
   });
+}
+
+//등록된 상품가져오기
+export async function getProducts() {
+  return get(ref(database, "products")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
+  });
+}
+
+//카트에 담긴 상품정보 가져오기
+export async function getCart(userId) {
+  return get(ref(database, `carts/${userId}`)) //
+    .then((snapshot) => {
+      const items = snapshot.val() || {};
+      return Object.values(items);
+    });
+}
+// 카트에 상품 추가및수정
+export async function addOrUpdateToCart(userId, product) {
+  return set(ref(database, `carts/${userId}/${product.id}`), product);
+}
+//카트에서 상품 삭제
+export async function removeFromCart(userId, productId) {
+  return remove(ref(database, `carts/${userId}/${productId}`));
 }
--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import Button from "../components/ui/Button";
 import { uploadImage } from "../api/uploader";
-import { addNewProduct } from "../api/firebase";
+import useProducts from "../hooks/useProducts";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
+
+  const { addProduct } = useProducts();
+  // const queryClient = useQueryClient;
+  // const addProduct = useMutation(
+  //   ({ product, url }) => addNewProduct(product, url),
+  //   {
+  //     onSuccess: () => queryClient.invalidateQueries(["products"]),
+  //   }
+  // );
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -23,13 +32,17 @@ export default function NewProduct() {
     uploadImage(file) //
       .then((url) => {
         console.log(url);
-        addNewProduct(product, url) //
-          .then(() => {
-            setSuccess("성공적으로 제품이 추가되었습니다.");
-            setTimeout(() => {
-              setSuccess(null);
-            }, 3000);
-          });
+        addProduct.mutate(
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess("성공적으로 제품이 추가되었습니다.");
+              setTimeout(() => {
+                setSuccess(null);
+              }, 3000);
+            },
+          }
+        );
       })
       .finally(() => setIsUploading(false));
 
