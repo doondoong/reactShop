87da4363e3ffe87f21c317467d22ce49e08af0ef
DEV.JS <pztdrd@nate.com>
2024-08-13 12:16:09 +0900

    경로수정 테스트

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./pages/ProtectedRoute";
 const router = createBrowserRouter([
   {
-    path: "/reactShop",
+    path: "/",
     element: <App />,
     errorElement: <NotFound />,
     children: [
