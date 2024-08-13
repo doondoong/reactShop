89b4af28159fd75f9bed999cf2d101ad8d3bd255
DEV.JS <pztdrd@nate.com>
2024-08-13 13:39:18 +0900

    환경변수 테스트

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -8,12 +8,12 @@ import {
 } from "firebase/auth";
 import { getDatabase, ref, get, set, remove } from "firebase/database";
 import { v4 as uuid } from "uuid";
-
+import { envVars } from "../vars";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_FIREBASE_PROJEXT_ID,
+  apiKey: envVars.REACT_APP_FIREBASE_API_KEY,
+  authDomain: envVars.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: envVars.REACT_APP_FIREBASE_FIREBASE_DB_URL,
+  projectId: envVars.REACT_APP_FIREBASE_FIREBASE_PROJEXT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- src/api/uploader.js
+++ src/api/uploader.js
@@ -1,8 +1,10 @@
+import { envVars } from "../vars";
+
 export async function uploadImage(file) {
   const data = new FormData();
   data.append("file", file);
-  data.append("upload_preset", process.env.REACT_APP_CLOUDINARY_PRESET);
-  return fetch(process.env.REACT_APP_CLOUDINARY_URL, {
+  data.append("upload_preset", envVars.REACT_APP_CLOUDINARY_PRESET);
+  return fetch(envVars.REACT_APP_CLOUDINARY_URL, {
     method: "POST",
     body: data,
   })
