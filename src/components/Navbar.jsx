943c77b57a783ac4d8ba6383d9eb3efde4769122
DEV.JS <pztdrd@nate.com>
2024-08-13 12:36:23 +0900

    루트경로 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,19 +11,19 @@ export default function Navbar() {
   const { user, login, logout } = useAuthContext();
   return (
     <header className="flex justify-between border-b border-gray-300 p-3">
-      <Link to="/" className="flex items-center text-4xl text-brand">
+      <Link to="/reactShop" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
-        <Link to="/products">Products</Link>
+        <Link to="products">Products</Link>
         {user && (
-          <Link to="/carts">
+          <Link to="carts">
             <CartStatus />
           </Link>
         )}
         {user && user.isAdmin && (
-          <Link to="/products/new" className="text-2xl">
+          <Link to="products/new" className="text-2xl">
             <BsFillPenFill />
           </Link>
         )}
--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 export default function ProtectedRoute({ children, requireAdmin }) {
   const { user } = useAuthContext();
   if (!user || (requireAdmin && !user.isAdmin)) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/reactShop" replace />;
   }
   return children;
 }
